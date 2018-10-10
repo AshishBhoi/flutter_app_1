77a2764a534bb3b8d14f5ef7fc3f7e9b4199c7b3
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2018-10-10 15:37:54 +0530

    Stateless Wideght

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    new Center(
-    child: new Text('this is my first flutter application', textDirection: TextDirection.ltr,)
-    )
-  );
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Stateless Wideght',
+      home: new Scaffold(
+        body: new Container(
+          color: Colors.pink,
+          child: new Container(
+            color: Colors.yellow,
+            margin: const EdgeInsets.all(30.0),
+          ),
+        ),
+      ),
+    );
+  }
 }
